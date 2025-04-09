d14b6c0d3f4dbf6cc7a27749ab59095138367d2c
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-09 14:12:15 +0530

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Asad Sayeed on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
