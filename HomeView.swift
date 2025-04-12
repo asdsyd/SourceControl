ba2313797ad7b75b6cc56e2c8f2d596c4cb2bcba
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-12 13:13:03 +0530

    Add Home View

---
+++ HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControl
+//
+//  Created by Asad Sayeed on 12/04/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
