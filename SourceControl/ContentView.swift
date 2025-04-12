8f5e81aacd9d5a48add39977eab44c2a007861a6
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-12 14:05:28 +0530

    [Clean] Add commit message documentation

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Asad Sayeed on 09/04/25.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] : Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
