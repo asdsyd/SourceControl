343e636c046f45792d1158561434e7b683197c13
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-12 12:58:55 +0530

    My first commit

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image(systemName: "scribble.variable")
+                .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Swiftful Thinking!")
         }
         .padding()
     }
