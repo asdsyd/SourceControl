6472e3e5536333545474e72d4dc6c3843a1ab57d
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-14 14:08:36 +0530

    [Feature] Add SettingsView

---
+++ SourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SourceControl
+//
+//  Created by Asad Sayeed on 14/04/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
