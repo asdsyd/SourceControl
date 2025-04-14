7e4a4236b2a767e10718c4aad04b2759ddfb7da0
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-14 14:21:07 +0530

    Fixed

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
                         Image(systemName: "globe")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Swiftful Thinking!!!!")
+                        Text("Swiftful Thinking")
                         
                         Button("Subscribe to me") {
                             
